6fedaa39fa65805ed302d211cc1d9a82c9a3e3d0
fajar hidayat <fajarh247@gmail.com>
2023-04-03 16:09:08 +0700

    deploy heroku

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,8 +1,14 @@
 const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const Conn = new Sequelize("nutech", "root", "", {
-	host: "localhost",
-	port: "3306",
+// const Conn = new Sequelize("nutech", "root", "", {
+// 	host: "localhost",
+// 	port: "3306",
+// 	dialect: "mysql",
+// });
+
+const dbUri = process.env.JAWSDB_URL;
+const Conn = new Sequelize(String(dbUri), {
 	dialect: "mysql",
 });
 
