6cd05a59b71abeb629b4f167f14894d14e1344c8
fajar hidayat <fajarh247@gmail.com>
2023-04-04 13:17:29 +0700

    cors add

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const cors = require("cors");
 
 var usersRouter = require("./routes/users");
 const authRouter = require("./routes/auth");
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", usersRouter);
